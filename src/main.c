14db6e16867922aa07fa77c6844e4f9ee4b200cc
denys <denysbonfim1@gmail.com>
2026-08-11 22:47:43 +0100

    setup: seeing race condition

--- src/main.c
+++ src/main.c
@@ -6,30 +6,42 @@
 /*   By: deferrei <deferrei@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/11 14:05:56 by deferrei          #+#    #+#             */
-/*   Updated: 2026/08/11 16:13:17 by deferrei         ###   ########.fr       */
+/*   Updated: 2026/08/11 21:54:43 by deferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/codexion.h"
 
+pthread_mutex_t mutex;
 
-void	*routine()
+
+
+void	*routine(void *str)
 {
-	printf("Test from threads\n");
-	sleep(3);
-	printf("Ending thread\n");
+	int *numero;
+	numero = (int *)str;
+	pthread_mutex_lock(&mutex);
+	if (*(numero) == 10000)
+	{
+		*(numero) -= 10000;
+		pthread_mutex_unlock(&mutex);
+		printf("Eu ganhei\n");
+	}
+	else
+		printf("Eu perdi\n");
 	return (NULL);
 }
 
 int	main(void)
 {
 	pthread_t t1, t2;
-	pthread_mutex_t mutex;
-	pthread_create(&t1, NULL, &routine, NULL);
-	pthread_mutex_lock(&mutex);
+	pthread_mutex_init(&mutex, NULL);
+	int primo;
+	primo = 10000;
+	pthread_create(&t1, NULL, &routine, &primo);
+	pthread_create(&t2, NULL, &routine, &primo);
 	pthread_join(t1, NULL);
-	pthread_mutex_unlock(&mutex);
-	pthread_create(&t2, NULL, &routine, NULL);
 	pthread_join(t2, NULL);
+
 	return (0);
 }
